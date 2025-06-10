27b482c6924c32d33bd278383b5dd07ef4ccf217
Bob Mannino <manninobobby@icloud.com>
2025-06-10 22:17:11 +0100

    button component

---
+++ src/lib/components/ui/button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  type Props = { children: Snippet } & HTMLButtonAttributes;
+
+  let { children, class: klass, ...props }: Props = $props();
+</script>
+
+<button
+  class={[
+    "flex items-center gap-1",
+    "rounded-3xl shadow-lg",
+    "text-primary-500 bg-white",
+    "group/button p-3",
+    "disabled:opacity-50",
+    klass,
+  ]}
+  {...props}
+>
+  {@render children()}
+</button>
