2a54c0123793f6a0e634f0c409967398b311e39d
Bob Mannino <manninobobby@icloud.com>
2025-06-10 22:17:15 +0100

    container component

---
+++ src/lib/components/ui/container.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type Props = { children: Snippet } & HTMLAttributes<HTMLDivElement>;
+
+  let { children, class: klass, ...props }: Props = $props();
+</script>
+
+<div class={["container mx-auto", klass]} {...props}>
+  {@render children()}
+</div>
