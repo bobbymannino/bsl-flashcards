394fea2a6963da7c97d782b4717f36807403976b
Bob Mannino <manninobobby@icloud.com>
2025-06-12 16:52:36 +0100

    feat: more themes
    resolves #1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
+  import Button from "$lib/components/ui/button.svelte";
+  import BrushIcon from "$lib/icons/brush-icon.svelte";
+  import { persistentState } from "$lib/stores.svelte";
+  import { onMount } from "svelte";
   import "../app.css";
   import Analytics from "./analytics.svelte";
 
   let { children } = $props();
+
+  let theme = persistentState("theme", "primary");
+
+  const themes = ["primary", "blue", "green", "purple", "gray"];
+  let themeIndex = 0;
+
+  function shuffleTheme() {
+    themeIndex += 1;
+    if (themeIndex >= themes.length) themeIndex = 0;
+    theme.value = themes[themeIndex];
+    document.body.dataset.theme = theme.value;
+  }
+
+  onMount(() => {
+    document.body.dataset.theme = theme.value;
+  });
 </script>
 
 <Analytics />
-{@render children()}
+<main>
+  {@render children()}
+
+  <Button title="Shuffle theme" onclick={shuffleTheme} class="fixed bottom-2 left-2">
+    <BrushIcon />
+  </Button>
+</main>
