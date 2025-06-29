3f637216a732fed7c8b023c2f9fea76bffc47338
Bob Mannino <manninobobby@icloud.com>
2025-06-29 12:27:49 +0100

    conversations page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,3 +24,32 @@ export const getRandomElement = <T>(array: T[]): T | undefined => {
   const randomIndex = generateRandomInt(0, array.length - 1);
   return array[randomIndex];
 };
+
+/**
+ * Generates a random number of elements from the given array. If the array
+ * length if 0 then an empty array will be returned. If the count is 0 or less
+ * an empty array will be returned. The same element may be returned multiple
+ * times.
+ *
+ * @param array - The array to select elements from
+ * @param count - The number of elements to generate
+ * @returns An array of randomly selected elements
+ *
+ * @example
+ * ```typescript
+ * const colors = ['red', 'blue', 'green'];
+ * getRandomElements(colors, 2); // Returns ['red', 'blue']
+ *
+ * getRandomElements([], 3); // Returns []
+ * ```
+ */
+export const getRandomElements = <T>(array: T[], count: number): T[] => {
+  if (array.length == 0 || count < 1) return [];
+
+  const result: T[] = [];
+  for (let i = 0; i < count; i++) {
+    const randomIndex = generateRandomInt(0, array.length - 1);
+    result.push(array[randomIndex]);
+  }
+  return result;
+};
